8611db600985e443ff9d002656a4e75ea1978836
HAMSIK Adam <adh@rsd.com>
2014-05-17 18:45:38 +0200

    Add proper setup.py

--- setup.py
+++ setup.py
@@ -1,67 +1,20 @@
 #!/usr/bin/env python
-"""
-py2app/py2exe build script for Loxodo.
-
-Usage (Mac OS X):
-    python setup.py py2app
-
-Usage (Windows):
-    python setup.py py2exe
-"""
-
 import sys
 from setuptools import setup
 
-if sys.platform == 'darwin':
-    extra_options = dict(
-        name="Loxodo",
-        setup_requires = ['py2app'],
-        app = ['loxodo.py'],
-        options = dict(
-            py2app = dict(
-                argv_emulation = True,
-                iconfile = 'resources/loxodo-icon.icns',
-                packages = ['src', 'wx'],
-                site_packages = True,
-                resources = ['resources', 'locale', 'LICENSE.txt', 'README.txt']
-            )
-        )
-    )
-    setup(**extra_options)
-elif sys.platform == 'win32':
-    import py2exe
-    import os
-
-    # create list of needed data files
-    dataFiles = []
-    for subdir in ('resources', 'locale'):
-        for root, dirs, files in os.walk(subdir):
-            if not files:
-                next
-            files = []
-            for filename in files:
-                files.append(os.path.join(root, filename))
-            if not files:
-                next
-            dataFiles.append((root, files))
-
-    extra_options = dict(
-        setup_requires = ['py2exe'],
-        windows = ['loxodo.py'],
-        data_files = dataFiles,
-        options = dict(
-            py2exe = dict(
-                excludes = 'ppygui'
-            )
-        )
-    )
-    setup(**extra_options)
-#else:
-#    extra_options = dict(
-#        name="Loxodo",
-#        app = ['loxodo.py'],
-#        scripts = ['loxodo.py']
-#    )
-#    setup(**extra_options)
+extra_options = dict(
+    name="Loxodo",
+    version="1.1",
+    description="CLI and web password manager. With v3 passwordsafe compatible DB.",
+    license = 'GPLv2',
+    author_email = 'haaaad@gmail.com',
+    author = "Christoph Sommer & Adam Hamsik ",
+    url = 'https://github.com/haad/loxodo',
+    include_package_data=True,
+    app = ['loxodo.py', 'web-loxodo.py'],
+    scripts = ['loxodo.py', 'web-loxodo.py'],
+    packages = ['Loxodo', 'Loxodo/db', 'Loxodo/twofish', 'Loxodo/frontends', 'Loxodo/frontends/cmdline', 'Loxodo/frontends/web']
+)
+setup(**extra_options)
 
 
